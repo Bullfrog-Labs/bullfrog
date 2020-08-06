8dceabaafc6092fc682dd733570b8188ee71c458
Leigh Stewart <leigh@macbook-pro.lan>
2020-08-06 09:57:31 -0700

    add signin

--- kmgmt/kmgmt-app/src/App.tsx
+++ kmgmt/kmgmt-app/src/App.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppContainer from "./components/AppContainer";
+import SignIn from "./components/SignIn";
+import Firebase from "./services/Firebase";
 import "./App.css";
 
+const firebase = Firebase.init();
+
 function App() {
-  return <AppContainer />;
+  const [userAuth, setUserAuth] = useState(firebase.auth().currentUser);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((firebaseUserAuth) => {
+      setUserAuth(firebaseUserAuth);
+    });
+  });
+
+  if (userAuth) {
+    console.log(`Logged in as ${userAuth.displayName}`);
+  } else {
+    console.log(`Not logged in`);
+  }
+
+  if (userAuth) {
+    return <AppContainer />;
+  } else {
+    return <SignIn />;
+  }
 }
 
 export default App;
