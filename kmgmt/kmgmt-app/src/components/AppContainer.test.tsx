b3030e7c36b59490b4bb82c99bf11d8542c3f4b2
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2020-08-07 14:39:33 -0700

    Fixing test name

--- kmgmt/kmgmt-app/src/components/AppContainer.test.tsx
+++ kmgmt/kmgmt-app/src/components/AppContainer.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import AppContainer from "./AppContainer";
 
-test("renders learn react link", () => {
+test("renders AppContainer", () => {
   const database = {
     getNotes: jest.fn(async () => [{ text: "Example note text" }]),
   };
