c8e8da0e1a20f69120fa770a12e9b0c2146e77c8
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2020-08-07 14:13:29 -0700

    Adding routing to from MainView to NoteView

--- kmgmt/kmgmt-app/src/components/AppContainer.tsx
+++ kmgmt/kmgmt-app/src/components/AppContainer.tsx
@@ -4,6 +4,8 @@ import { MuiThemeProvider } from "@material-ui/core";
 import { createMuiTheme } from "@material-ui/core/styles";
 import MainView from "./MainView";
 import { Database } from "../services/Database";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import NoteView from "./NoteView";
 
 const theme = createMuiTheme({
   direction: "ltr",
@@ -15,10 +17,17 @@ const theme = createMuiTheme({
 export default function AppContainer(props: { database: Database }) {
   return (
     <MuiThemeProvider theme={theme}>
-      <Container maxWidth="md">
-        {/* Replace MainView completely with the real component. */}
-        <MainView database={props.database} />
-      </Container>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/notes/:id">
+            <NoteView database={props.database} />
+          </Route>
+          <Route path="/">
+            {/* Replace MainView completely with the real component. */}
+            <MainView database={props.database} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </MuiThemeProvider>
   );
 }
