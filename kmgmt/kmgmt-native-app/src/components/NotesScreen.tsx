a3c9ce7341b6416282992835aa434b5e04426de3
Leigh Stewart <leigh@macbook-pro.lan>
2020-08-13 11:38:21 -0700

    break up

---
+++ kmgmt/kmgmt-native-app/src/components/NotesScreen.tsx
@@ -0,0 +1,62 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+import "./services/Firebase";
+import firebase from "firebase";
+import { StyleSheet, View, StatusBar, FlatList, Text } from "react-native";
+import { NotesScreenNavigationProp } from "../services/Navigation";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
+
+const firestore = firebase.firestore();
+
+export default function NotesScreen(props: {
+  navigation: NotesScreenNavigationProp;
+}) {
+  const [notes, setNotes] = React.useState<{ title: string; body: string }[]>(
+    []
+  );
+  React.useEffect(() => {
+    async function getNotes() {
+      const email = firebase.auth().currentUser?.email;
+      console.log(`using email for fetch; email = ${email}`);
+      if (email) {
+        const coll = await firestore
+          .collection("users")
+          .doc(email)
+          .collection("notes")
+          .get();
+        const fetchedNotes = coll.docs.map((doc) => {
+          return {
+            title: doc.data().title,
+            body: doc.data().body,
+          };
+        });
+        console.log(`notes = ${fetchedNotes}`);
+        setNotes(fetchedNotes);
+      }
+    }
+    getNotes();
+  }, [firestore, firebase]);
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={notes}
+        renderItem={({ item }) => <Text style={styles.item}>{item.body}</Text>}
+      />
+      <StatusBar />
+    </View>
+  );
+}
---
+++ kmgmt/kmgmt-native-app/src/services/Navigation.ts
@@ -0,0 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from "@react-navigation/stack";
+
+// Param list
+export type StackParamList = {
+  Login: undefined;
+  Notes: undefined;
+};
+
+export const Stack = createStackNavigator<StackParamList>();
+export type NotesScreenNavigationProp = StackNavigationProp<
+  StackParamList,
+  "Notes"
+>;
+export type LoginScreenNavigationProp = StackNavigationProp<
+  StackParamList,
+  "Login"
+>;
