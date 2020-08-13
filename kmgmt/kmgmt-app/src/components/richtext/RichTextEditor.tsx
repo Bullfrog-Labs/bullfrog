d034e9897d37bca227fabd7cbca386897e352e4d
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2020-08-13 10:54:27 -0700

    Pared down RichTextEditor

--- kmgmt/kmgmt-app/src/components/richtext/RichTextEditor.tsx
+++ kmgmt/kmgmt-app/src/components/richtext/RichTextEditor.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useMemo } from "react";
-import { ReactEditor, withReact, Slate, Editable, useSlate } from "slate-react";
-import { createEditor, Editor, Text, Transforms } from "slate";
+import { ReactEditor, withReact, Slate, Editable } from "slate-react";
+import { createEditor } from "slate";
 import { withHistory } from "slate-history";
 
 import {
@@ -10,11 +10,7 @@ import {
   Divider,
   Typography,
 } from "@material-ui/core";
-import {
-  Node as SlateNode,
-  Element as SlateElement,
-  Editor as SlateEditor,
-} from "slate";
+import { Node as SlateNode } from "slate";
 
 import { hotkeyHandler } from "./EventHandling";
 
@@ -23,93 +19,6 @@ import { hotkeyHandler } from "./EventHandling";
 
 // The implementation below is based off of
 // https://github.com/ianstormtaylor/slate/blob/master/site/examples/richtext.js.
-
-// TODO: need to make it so that inserting a break from inside a heading does
-// not cause the heading itself to become a paragraph.
-const withResetBlockOnInsertBreak = (editor) => {
-  const { insertBreak } = editor;
-
-  editor.insertBreak = () => {
-    insertBreak();
-
-    for (let block in HEADING_BLOCKS.filter((x) => isBlockActive(editor, x))) {
-      toggleBlock(editor, block);
-    }
-  };
-
-  return editor;
-};
-
-const withEditableTypographyLayout = (editor) => {
-  const { normalizeNode } = editor;
-
-  editor.normalizeNode = (entry) => {
-    const [node, path] = entry;
-
-    if (Text.isText(node)) {
-      // Make sure that there are no newlines in the text
-      const newNode = {
-        text: node.text.replace(/\r?\n|\r/g, ""),
-      };
-      Transforms.setNodes(editor, newNode, { at: path });
-    } else {
-      // Merge all nodes into a single text node
-      if (path.length > 0 && path[path.length - 1] !== 0) {
-        Transforms.mergeNodes(editor, { at: path, voids: true });
-      }
-    }
-
-    return normalizeNode(entry);
-  };
-
-  return editor;
-};
-
-const EditableTypography = ({ initialValue, variant, handleEscape }) => {
-  if (initialValue === undefined) {
-    initialValue = [
-      {
-        type: "paragraph",
-        children: [{ text: "" }],
-      },
-    ];
-  }
-  const [value, setValue] = useState(initialValue);
-  const editor = useMemo(
-    () => withEditableTypographyLayout(withHistory(withReact(createEditor()))),
-    []
-  );
-
-  const renderLeaf = useCallback(
-    ({ children, attributes }) => (
-      <Typography variant={variant} {...attributes}>
-        {children}
-      </Typography>
-    ),
-    [variant]
-  );
-
-  return (
-    <Slate editor={editor} value={value} onChange={setValue}>
-      <Editable
-        placeholder="Enter a title"
-        renderLeaf={renderLeaf}
-        onKeyDown={(event) => {
-          if (event.key === "Enter" || event.key === "Escape") {
-            event.preventDefault();
-            if (handleEscape) {
-              handleEscape(event);
-            }
-          }
-        }}
-      />
-    </Slate>
-  );
-};
-
-const DocumentTitle = (props) => {
-  return <EditableTypography variant="h1" {...props} />;
-};
 
 type Title = string | undefined;
 type Body = SlateNode[];
