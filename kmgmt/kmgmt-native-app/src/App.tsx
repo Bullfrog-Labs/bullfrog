7a17cc2ca128425c4b2c230c08e2f718f870aba2
Leigh Stewart <leigh@macbook-pro.lan>
2020-08-14 17:01:57 -0700

    add, yarn workspaces, shared module common, expo yarn workspaces support

--- kmgmt/kmgmt-native-app/src/App.tsx
+++ kmgmt/kmgmt-native-app/src/App.tsx
@@ -1,4 +1,5 @@
 import "react-native-gesture-handler";
+import * as log from "loglevel";
 import { registerRootComponent } from "expo";
 import * as React from "react";
 import "./services/Firebase";
@@ -6,8 +7,13 @@ import LoginScreen from "./components/LoginScreen";
 import NotesScreen from "./components/NotesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { Stack } from "./services/Navigation";
+import { Logging } from "kmgmt-common";
+
+Logging.configure(log);
 
 export function App() {
+  const logger = log.getLogger("App");
+  logger.debug("foo");
   return (
     <NavigationContainer>
       <Stack.Navigator>
