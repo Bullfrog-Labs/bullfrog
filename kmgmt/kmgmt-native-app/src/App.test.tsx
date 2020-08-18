ad19ef66555b4a33ca3316fb8690d73d1d7ed20a
Leigh Stewart <leigh@macbook-pro.lan>
2020-08-18 10:58:58 -0700

    add logging dep

--- kmgmt/kmgmt-native-app/src/App.test.tsx
+++ kmgmt/kmgmt-native-app/src/App.test.tsx
@@ -1,9 +1,15 @@
+import * as log from "loglevel";
 import "react-native-gesture-handler";
 import React from "react";
 import { render } from "@testing-library/react-native";
 import { View, Text } from "react-native";
+import { Logging } from "kmgmt-common";
+
+Logging.configure(log);
+const logger = log.getLogger("App.test");
 
 test("test dummy react to prove it works", () => {
+  logger.debug("running test!");
   const view = (
     <View>
       <Text>Login</Text>
