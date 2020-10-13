6b027e5782d1acfc177371b5d09c48ae5ab25828
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2020-10-12 17:49:33 -0700

    silly fix

--- kmgmt/kmgmt-app/src/components/richtext/EditorBehaviors.ts
+++ kmgmt/kmgmt-app/src/components/richtext/EditorBehaviors.ts
@@ -1,16 +1,19 @@
 import { Editor, Text, Transforms } from "slate";
-import { SECTION_BLOCKS } from "./Types";
+import { STRUCTURE_BLOCKS } from "./Types";
 import { isBlockActive, toggleBlock } from "./Blocks";
 
-// Ensures that inserting a break in the middle of a section block (i.e.
-// headings) causes the new block to be of the same type of section block.
+// Ensures that inserting a break in the middle of a structure block (i.e.
+// headings) causes the new block to be of the same type of structure block.
 export const withResetBlockOnInsertBreak = (editor: Editor) => {
   const { insertBreak } = editor;
 
   editor.insertBreak = () => {
     insertBreak();
 
-    for (let block in SECTION_BLOCKS.filter((x) => isBlockActive(editor, x))) {
+    // TODO: Figure out if this behavior makes sense, or what it even means
+    for (let block in STRUCTURE_BLOCKS.filter((x) =>
+      isBlockActive(editor, x)
+    )) {
       toggleBlock(editor, block);
     }
   };
