b97f96148a53e6a10f87fced4e821eb554732545
Leigh Stewart <leigh@macbook-pro.lan>
2020-10-26 10:25:22 -0700

    add fetch script

--- bfr/bookmarks-sync/fetch_bookmarks.py
+++ bfr/bookmarks-sync/fetch_bookmarks.py
@@ -7,68 +7,47 @@ import time
 from threading import Thread
 import cgi
 
-consumer_key = "93907-4bc0f7edcc3af162423e8b53"
+
+class Handler(BaseHTTPRequestHandler):
+    logger = logging.getLogger("Handler")
+
+    def do_GET(self):
+        self.logger.debug(
+            f"got redirect; path={self.path}, " +
+            f"content_type={self.headers.get_content_type()}")
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(
+            "<html><body><p>You are now logged in.</p></body></html>".encode())
+
+        # Has to happen on a separate thread, else deadlock.
+        def shutdown():
+            time.sleep(5)
+            self.server.shutdown()
+
+        t = Thread(target=shutdown)
+        t.start()
 
 
 class OAuthRedirectServer(object):
     logger = logging.getLogger("OAuthRedirectServer")
-    access_token = None
-    server = None
 
-    def wait_for_token(self):
-        def set_token(access_token):
-            self.access_token = access_token
-
-        class Handler(BaseHTTPRequestHandler):
-            logger = logging.getLogger("Handler")
-
-            def do_GET(self):
-                self.logger.debug("got post")
-
-                self.logger.debug(self.headers.get_payload())
-                # self.logger.debug(self.headers.get_all())
-                self.logger.debug(self.headers.get_content_type())
-                self.logger.debug(self.headers.get_params())
-                self.logger.debug(self.path)
-                self.logger.debug(self.requestline)
-
-                set_token("foo")
-                self.send_response(200)
-
-                def shutdown():
-                    time.sleep(5)
-                    self.server.shutdown()
-
-                t = Thread(target=shutdown)
-                t.start()
-
-            def do_POST(self):
-                self.logger.debug("got post")
-
-                self.logger.debug(self.headers)
-                self.logger.debug(self.headers['content-type'])
-                self.logger.debug(self.path)
-                self.logger.debug(self.requestline)
-
-                set_token("foo")
-
-                def shutdown():
-                    self.server.shutdown()
-
-                t = Thread(target=shutdown)
-                t.start()
-
+    def wait_for_redirect(self):
         self.server = HTTPServer(('', 8000), Handler)
-        self.logger.debug("serving...")
+        self.logger.debug("Waiting for redirect...")
         self.server.serve_forever()
-
-        return self.access_token
+        self.logger.debug("Done")
 
 
 def main():
+    default_consumer_key = "93907-4bc0f7edcc3af162423e8b53"
     parser = argparse.ArgumentParser()
     parser.add_argument("--log-level", type=str, default="INFO")
     parser.add_argument("--access-token", type=str, required=False)
+    parser.add_argument("--consumer-key", type=str,
+                        required=False, default=default_consumer_key)
     parser.add_argument("--login",
                         action="store_true", required=False)
     args = parser.parse_args()
@@ -80,28 +59,32 @@ def main():
         raise Exception(
             "Either pass in your access token or specify --login to log in again")
 
+    access_token = args.access_token
+    consumer_key = args.consumer_key
+
     if (args.login):
         server = OAuthRedirectServer()
-        logger.debug("Waiting...")
-
+        logger.debug(
+            "Login requested, logging in with consumer_key=" + consumer_key)
         request_token = Pocket.get_request_token(
             consumer_key=consumer_key, redirect_uri="http://localhost:8000/")
         logger.debug(f"Got rt {request_token}")
         auth_url = Pocket.get_auth_url(
             code=request_token, redirect_uri="http://localhost:8000/")
         logger.debug(f"Got auth url {auth_url}")
-
-        token = server.wait_for_token()
-
-        logger.debug("Got token " + token)
+        server.wait_for_redirect()
 
         user_credentials = Pocket.get_credentials(
             consumer_key=consumer_key, code=request_token)
-
         logger.debug("Got creds " + str(user_credentials))
+        access_token = user_credentials['access_token']
 
-        # auth_url = Pocket.get_auth_url(
-        #    code=request_token, redirect_uri=redirect_uri)
+    logger.debug(
+        f"Logged in; access_token={access_token}, consumer_key={consumer_key}")
+
+    pocket = Pocket(consumer_key, access_token)
+    results = pocket.get(count=1)
+    print(results[0]['list'])
 
 
 main()
