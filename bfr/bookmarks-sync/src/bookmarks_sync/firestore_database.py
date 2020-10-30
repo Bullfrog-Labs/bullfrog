2a28dd12dcdae6f9e9580c6d9763db0dbea0a36d
Leigh Stewart <leigh@macbook-pro.lan>
2020-10-29 17:29:54 -0700

    script works

--- bfr/bookmarks-sync/src/bookmarks_sync/firestore_database.py
+++ bfr/bookmarks-sync/src/bookmarks_sync/firestore_database.py
@@ -30,6 +30,14 @@ class BookmarkRecord(TypedDict):
   updated_at: Optional[datetime]
 
 
+class UserRecord(TypedDict):
+  user_name: str
+  pocket_access_token: str
+  pocket_sync_enabled: bool
+  created_at: Optional[datetime]
+  updated_at: Optional[datetime]
+
+
 class FirestoreDatabase(object):
   def __init__(self, db):
     self.logger = logging.getLogger("FirestoreDatabase")
@@ -66,10 +74,21 @@ class FirestoreDatabase(object):
       )
       self.logger.debug(f"done; result={result}")
 
+  def update_user(self, user_name: str, user_record: UserRecord):
+    if self.db.collection("users").document(user_name).get().exists:
+      self.apply_updated_record_timestamps(user_record)
+      self.db.collection("users").document(user_name).update(user_record)
+    else:
+      self.apply_new_record_timestamps(user_record)
+      self.db.collection("users").document(user_name).set(user_record)
+
   # Helper functions
   def apply_new_record_timestamps(self, record):
     record["created_at"] = datetime.now()
     record["updated_at"] = record["created_at"]
+
+  def apply_updated_record_timestamps(self, record):
+    record["updated_at"] = datetime.now()
 
   # Named constructors
   @classmethod
--- bfr/bookmarks-sync/src/tools/update_user.py
+++ bfr/bookmarks-sync/src/tools/update_user.py
@@ -1,6 +1,10 @@
 import logging
 import argparse
 from bookmarks_sync.firestore_database import FirestoreDatabase
+from bookmarks_sync.firebase_app import FirebaseApp
+
+
+project_id = "bullfrog-reader"
 
 
 def main():
@@ -13,15 +17,32 @@ def main():
     help="User name to update",
   )
   parser.add_argument(
-    "--access-token",
+    "--pocket-access-token",
     type=str,
-    required=True,
+    required=False,
     help="Pocket access token to use",
+  )
+  parser.add_argument(
+    "--pocket-sync-enabled",
+    action="store_true",
+    required=False,
+    help="Enable pocket sync",
   )
   args = parser.parse_args()
   logging.basicConfig(level=args.log_level)
   logger = logging.getLogger("update_user")
   logger.debug("Starting...")
+
+  app = FirebaseApp.admin(project_id)
+  db = FirestoreDatabase.admin(app)
+
+  user_record = {"user_name": args.user_name}
+  if args.pocket_sync_enabled:
+    user_record["pocket_sync_enabled"] = args.pocket_sync_enabled
+  if args.pocket_access_token:
+    user_record["pocket_access_token"] = args.pocket_access_token
+
+  db.update_user(args.user_name, user_record)
 
 
 main()
