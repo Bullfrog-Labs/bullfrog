578b607dfd54f7742561f02d0cb92df2a3c1af4e
Leigh Stewart <leigh@macbook-pro.lan>
2020-12-01 17:46:44 -0800

    add sim

--- threads-analysis/src/extract_spacy_entities.py
+++ threads-analysis/src/extract_spacy_entities.py
@@ -5,6 +5,18 @@ import os
 from urllib.parse import urlparse
 import re
 import spacy
+import time
+import base64
+from spacy.tokens import Doc
+
+
+timers = {}
+
+
+def log_timers(timers):
+  for metric_name in dict(filter(lambda t: t[0].startswith("elapsed"), timers.items())):
+    elapsed_ms = timers[metric_name]
+    logger.debug(f"{metric_name} = {elapsed_ms}s")
 
 
 def save_entities(entities_doc, base_dir, article_id):
@@ -25,10 +37,16 @@ def main():
   logger = logging.getLogger("extract_entities")
   logger.debug("starting...")
 
-  # need to run this first: python3 -m spacy download en_core_web_sm
-  nlp = spacy.load("en_core_web_sm")
+  timers["start_ms"] = time.time()
 
+  # need to run this first: python3 -m spacy download en_core_web_sm
+  timers["start_load_ms"] = time.time()
+  nlp = spacy.load("en_core_web_lg")
+  timers["elapsed_load_ms"] = time.time() - timers["start_load_ms"]
+
+  timers["start_read_doc_ms"] = time.time()
   article_doc = json.loads(open(args.article_doc).read())
+  timers["elapsed_read_doc_ms"] = time.time() - timers["start_read_doc_ms"]
 
   logger.debug(f"loaded article doc {article_doc['id']}")
 
@@ -36,17 +54,33 @@ def main():
   if article_text is None:
     raise Exception("missing text for doc, aborting")
 
+  timers["start_nlp_doc_ms"] = time.time()
   doc = nlp(article_text)
+  timers["elapsed_nlp_doc_ms"] = time.time() - timers["start_nlp_doc_ms"]
+
   logger.debug(f"got ents; num={len(doc.ents)}")
   for ent in doc.ents:
     print(f"{ent.label_} | {ent.text}".strip())
+  logger.debug(f"tensor={doc.vector}")
 
-  entities_doc = doc.to_json()
-  entities_doc["bullfrog_id"] = article_doc['id']
+  doc_data_str = str(base64.b64encode(doc.to_bytes()), 'UTF-8')
+
+  entities_doc = {
+    "bullfrog_id": article_doc['id'],
+    "spacy_doc": doc_data_str,
+  }
+
+  doc_loader = Doc(nlp.vocab)
+  b64_doc_a = entities_doc["spacy_doc"]
+  bytes_doc_a = base64.b64decode(b64_doc_a.encode())
+  spacy_doc_a = doc_loader.from_bytes(bytes_doc_a)
 
   logger.debug(f"writing entities")
+  timers["start_save_doc_ms"] = time.time()
   save_entities(entities_doc, args.dest, article_doc["id"])
+  timers["elapsed_save_doc_ms"] = time.time() - timers["start_save_doc_ms"]
   logger.debug(f"done")
+  log_timers(timers)
 
 
 main()
---
+++ threads-analysis/src/gen_sim_spacy.py
@@ -0,0 +1,83 @@
+import argparse
+import logging
+import json
+import os
+import spacy
+from spacy.tokens import Doc
+import base64
+import time
+
+timers = {}
+
+
+def log_timers(timers):
+  logger = logging.getLogger("gen_sim_spacy")
+  for metric_name in dict(filter(lambda t: t[0].startswith("elapsed"), timers.items())):
+    elapsed_ms = timers[metric_name]
+    logger.debug(f"{metric_name} = {elapsed_ms}s")
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--log-level", type=str, default="INFO")
+  parser.add_argument("--entity-doc-a", type=str, required=True)
+  parser.add_argument("--entity-doc-b", type=str, required=True)
+  parser.add_argument("--data-dir", type=str, required=True)
+
+  args = parser.parse_args()
+  logging.basicConfig(level=args.log_level)
+  logger = logging.getLogger("gen_sim_spacy")
+  logger.debug("Starting...")
+
+  timers["start_load_files_ms"] = time.time()
+  doc_a = json.loads(open(args.entity_doc_a).read())
+  doc_a_id = doc_a['bullfrog_id']
+  logger.debug(f"loaded entity doc a {doc_a_id}")
+  doc_b = json.loads(open(args.entity_doc_b).read())
+  doc_b_id = doc_b['bullfrog_id']
+  logger.debug(f"loaded entity doc b {doc_b_id}")
+  timers["elapsed_load_files_ms"] = time.time() - timers["start_load_files_ms"]
+
+  timers["start_load_model_ms"] = time.time()
+  nlp = spacy.load("en_core_web_lg")
+  timers["elapsed_load_model_ms"] = time.time() - timers["start_load_model_ms"]
+
+  timers["start_decode_ms"] = time.time()
+  b64_doc_a = doc_a["spacy_doc"]
+  bytes_doc_a = base64.b64decode(b64_doc_a.encode())
+  b64_doc_b = doc_b["spacy_doc"]
+  bytes_doc_b = base64.b64decode(b64_doc_b.encode())
+  timers["elapsed_decode_ms"] = time.time() - timers["start_decode_ms"]
+
+  timers["start_doc_load_ms"] = time.time()
+  doc_loader_a = Doc(nlp.vocab)
+  spacy_doc_a = doc_loader_a.from_bytes(bytes_doc_a)
+  logger.debug(len(bytes_doc_b))
+  doc_loader_b = Doc(nlp.vocab)
+  spacy_doc_b = doc_loader_b.from_bytes(bytes_doc_b)
+  timers["elapsed_doc_load_ms"] = time.time() - timers["start_doc_load_ms"]
+
+  timers["start_sim_ms"] = time.time()
+  similarity = spacy_doc_a.similarity(spacy_doc_b)
+  logger.debug(f"similarity={similarity}")
+  timers["elapsed_sim_ms"] = time.time() - timers["start_sim_ms"]
+
+  sim_spacy_id = f"{doc_a_id}-{doc_b_id}"
+
+  sim_spacy_doc = {
+    "id": sim_spacy_id,
+    "similarity": similarity,
+  }
+
+  sim_spacy_dirname = os.path.join(args.data_dir, "sim_spacy")
+  os.makedirs(sim_spacy_dirname, exist_ok=True)
+  similarity_filename = os.path.join(
+    sim_spacy_dirname, f"{sim_spacy_id}.json")
+  logger.debug(f"writing to {similarity_filename}")
+  with open(similarity_filename, 'w') as similarity_file:
+    json.dump(sim_spacy_doc, similarity_file)
+
+  log_timers(timers)
+
+
+main()
