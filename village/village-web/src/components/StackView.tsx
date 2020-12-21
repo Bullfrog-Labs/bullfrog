8cedbc8cf3079651f11e56af71c0a0fd6b7f9cf5
Leigh Stewart <leigh@macbook-pro.lan>
2020-12-21 15:03:44 -0800

    cleanup

--- village/village-web/src/components/StackView.tsx
+++ village/village-web/src/components/StackView.tsx
@@ -1,13 +1,20 @@
 import * as log from "loglevel";
 import React, { useState, useContext } from "react";
-import { Container, Typography } from "@material-ui/core";
+import {
+  Avatar,
+  Container,
+  ListItemAvatar,
+  Typography,
+} from "@material-ui/core";
 import { AuthContext } from "../services/auth/Auth";
-import { UserPost, Source } from "../services/VillageController";
+import { UserPost, Source, userPosts0 } from "../services/VillageController";
 import Divider from "@material-ui/core/Divider";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import ImageIcon from "@material-ui/icons/Image";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -20,7 +27,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export function useStackState(sourceName: string) {
-  const [posts, setPosts] = useState<UserPost[]>([]);
+  const [posts, setPosts] = useState<UserPost[]>(userPosts0);
   const [source, setSource] = useState<Source>({ name: sourceName });
   return { posts: posts, source: source };
 }
@@ -41,10 +48,24 @@ export function StackView(props: { posts: UserPost[]; source: Source }) {
   const classes = useStyles();
 
   const listItems = posts.map((post) => {
+    const linkTo = `/profile/${post.user.uid}`;
+    const listItemPrimaryText = (
+      <Typography variant="body2">
+        {post.post.title} -{" "}
+        <em>
+          <Link to={linkTo}>{post.user.username}</Link>
+        </em>
+      </Typography>
+    );
     return (
       <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt={post.user.displayName}>
+            <ImageIcon />
+          </Avatar>
+        </ListItemAvatar>
         <ListItemText
-          primary={post.post.title}
+          primary={listItemPrimaryText}
           secondary={<React.Fragment>{post.post.body}</React.Fragment>}
         />
       </ListItem>
