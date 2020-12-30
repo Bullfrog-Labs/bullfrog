08b7e2d1e942a4e4713d2b4515b1ede0b2f71748
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2020-12-30 12:52:21 -0800

    Nybbles/links Pulling DocumentTitle out of RichTextEditor (#57)
    - Separated DocumentTitle from RichTextEditor.
    - Refactored BasePostView rendering.
    - Created a separate method for focusing the editor.
    - Turned RichTextEditor into a class component.

--- village/village-web/src/components/richtext/RichTextEditor.test.tsx
+++ village/village-web/src/components/richtext/RichTextEditor.test.tsx
@@ -1,37 +1,21 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import RichTextEditor, {
-  EMPTY_RICH_TEXT_STATE,
-  Title,
-  Body,
-} from "./RichTextEditor";
+import RichTextEditor, { Body } from "./RichTextEditor";
+import { EMPTY_RICH_TEXT } from "./Utils";
 
 // TODO: Figure out how to test RichTextEditor more deeply. It might be that
 // js-dom is insufficient to test it, i.e. an actual browser is needed.
 
 test("renders RichTextEditor", () => {
-  let title = EMPTY_RICH_TEXT_STATE.title;
-  const setTitle = (newTitle: Title) => {
-    title = newTitle;
-  };
-
-  let body = EMPTY_RICH_TEXT_STATE.body;
+  let body = EMPTY_RICH_TEXT;
   const setBody = (newBody: Body) => {
     body = newBody;
   };
 
   const { getByText } = render(
-    <RichTextEditor
-      title={title}
-      onTitleChange={setTitle}
-      body={body}
-      onBodyChange={setBody}
-    />
+    <RichTextEditor body={body} onChange={setBody} />
   );
 
-  const documentTitleElement = getByText("Enter a title");
   const documentBodyElement = getByText("Enter some text");
-
-  expect(documentTitleElement).toBeInTheDocument();
   expect(documentBodyElement).toBeInTheDocument();
 });
