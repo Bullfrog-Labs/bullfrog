003cac2704b9367388a087dc78c233e23b34d753
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-01-05 01:21:16 -0800

    Adding failing test for navigating from PostView to PostView

--- village/village-web/src/views/PostView.test.tsx
+++ village/village-web/src/views/PostView.test.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import { CreateNewPostView, PostView } from "./PostView";
-import { EMPTY_RICH_TEXT } from "../components/richtext/Utils";
-import { MemoryRouter } from "react-router-dom";
+import { screen, render, waitFor } from "@testing-library/react";
+import { CreateNewPostView, PostView, PostViewController } from "./PostView";
+import {
+  EMPTY_RICH_TEXT,
+  stringToSlateNode,
+} from "../components/richtext/Utils";
+import { MemoryRouter, Route, Router } from "react-router-dom";
 import { MentionNodeData } from "@blfrg.xyz/slate-plugins";
+
+import { createMemoryHistory } from "history";
+import { PostId } from "../services/store/Posts";
+import { GetUserFn, UserId, UserRecord } from "../services/store/Users";
 
 test("Renders CreateNewPostView", () => {
   const createPost = jest.fn();
@@ -27,6 +34,18 @@ test("Renders CreateNewPostView", () => {
   expect(titleEl).toBeInTheDocument();
 });
 
+const viewer: UserRecord = {
+  uid: "456",
+  displayName: "baz",
+  username: "baz",
+};
+
+const author: UserRecord = {
+  uid: "123",
+  displayName: "qux",
+  username: "qux",
+};
+
 test("Renders PostView", () => {
   const props = {
     readOnly: false,
@@ -36,16 +55,8 @@ test("Renders PostView", () => {
       title: "bar",
       body: EMPTY_RICH_TEXT,
     },
-    viewer: {
-      uid: "456",
-      displayName: "baz",
-      username: "baz",
-    },
-    author: {
-      uid: "123",
-      displayName: "qux",
-      username: "qux",
-    },
+    viewer: viewer,
+    author: author,
 
     getTitle: jest.fn(),
     renamePost: jest.fn(),
@@ -67,4 +78,58 @@ test("Renders PostView", () => {
 
   const authorEl = getByText("qux");
   expect(authorEl).toBeInTheDocument();
+});
+
+test("PostView to PostView navigation works", async () => {
+  const history = createMemoryHistory();
+  const posts = [
+    {
+      id: "abc",
+      authorId: author.uid,
+      body: EMPTY_RICH_TEXT,
+      title: "Foo",
+    },
+    {
+      id: "def",
+      authorId: author.uid,
+      body: stringToSlateNode("Non-empty"),
+      title: "Bar",
+    },
+  ];
+
+  const postsMap = new Map(posts.map((post) => [post.id, post]));
+
+  const getUser: GetUserFn = jest.fn(async () => author);
+  const getPost = jest.fn(async (uid: UserId, postId: PostId) => {
+    return postsMap.get(postId);
+  });
+
+  const getGlobalMentions = jest.fn(async (titlePrefix: string) => {
+    return posts;
+  });
+
+  const props = {
+    viewer: viewer,
+    getUser: getUser,
+    getPost: getPost,
+    getGlobalMentions: getGlobalMentions,
+    renamePost: jest.fn(),
+    syncBody: jest.fn(),
+    createPost: jest.fn(),
+  };
+
+  history.push(`/post/${author.uid}/abc`);
+
+  render(
+    <Router history={history}>
+      <Route exact path="/post/:authorId/:postId">
+        <PostViewController {...props} />
+      </Route>
+    </Router>
+  );
+
+  await waitFor(() => screen.getByText("Foo"));
+
+  history.push(`/post/${author.uid}/def`);
+  await waitFor(() => screen.getByText("Bar"));
 });
