30cd68adf7887f79cd9eaf24640244a13cdb3b7b
Leigh Stewart <leighst@users.noreply.github.com>
2021-01-06 08:15:52 -0800

    bugs and style change (#59)

--- village/village-web/src/components/richtext/DocumentTitle.tsx
+++ village/village-web/src/components/richtext/DocumentTitle.tsx
@@ -5,7 +5,7 @@ import {
 } from "./EditableTypography";
 
 const DocumentTitle = (props: EditableTypographyProps) => {
-  return <EditableTypography variant="h1" {...props} />;
+  return <EditableTypography variant="h4" {...props} />;
 };
 
 export default DocumentTitle;
