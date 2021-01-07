25b41586ef65410752d0abe3fe7922685bc7bbfa
Leigh Stewart <leigh@macbook-pro.lan>
2021-01-07 14:45:34 -0800

    fix issues

--- village/village-web/src/components/richtext/Types.ts
+++ village/village-web/src/components/richtext/Types.ts
@@ -2,4 +2,6 @@ import { Node as SlateNode } from "slate";
 
 export type Platform = "generic" | "apple" | "windows";
 
+export type KBEventHandler = (event: KeyboardEvent) => void;
+
 export type RichText = SlateNode[];
