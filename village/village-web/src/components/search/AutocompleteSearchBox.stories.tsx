5c5e69cb633d375f29446136e2536387f2cfd51b
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-01-07 17:05:24 -0800

    Still cannot find react-autosuggest module

---
+++ village/village-web/src/components/search/AutocompleteSearchBox.stories.tsx
@@ -0,0 +1,18 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import React from "react";
+import {
+  AutocompleteSearchBox,
+  AutocompleteSearchBoxProps,
+} from "./AutocompleteSearchBox";
+
+export default {
+  title: "Search/AutocompleteSearchBox",
+  component: AutocompleteSearchBox,
+} as Meta;
+
+const Template: Story<AutocompleteSearchBoxProps> = (args) => (
+  <AutocompleteSearchBox {...args} />
+);
+
+export const InitialState = Template.bind({});
+InitialState.args = {};
--- village/village-web/src/components/search/AutocompleteSearchBox.tsx
+++ village/village-web/src/components/search/AutocompleteSearchBox.tsx
@@ -1,3 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Autosuggest from "react-autosuggest";
 
-// AutoC
+type SearchSuggestion = {};
+
+type SuggestionFetchFn = (value: string) => SearchSuggestion[];
+type AutocompleteSearchBoxOnChangeFn = (event: Event, newValue: string) => void;
+type AutocompleteSearchBoxFetchRequestedFn = (value: String) => void;
+
+export type AutocompleteSearchBoxProps = {
+  getSuggestions: SuggestionFetchFn;
+};
+
+type AutocompleteSearchBoxInputProps = {
+  value: string;
+  onChange: AutocompleteSearchBoxOnChangeFn;
+};
+
+export const AutocompleteSearchBox = (props: AutocompleteSearchBoxProps) => {
+  const [value, setValue] = useState<string>("");
+  const [suggestions, setSuggestions] = useState<SearchSuggestion[]>([]);
+
+  const onChange: AutocompleteSearchBoxOnChangeFn = (event, newValue) => {
+    setValue(newValue);
+  };
+  /*
+  const onSuggestionsFetchRequested = async (value) => {
+    const suggestions = await props.getSuggestions(value);
+    setSuggestions(suggestions);
+  };
+
+  const onSuggestionsClearRequested = undefined;
+
+  const inputProps = {
+    value: value,
+    onChange: onChange,
+  };
+
+  return <Autosuggest suggestions={suggestions} />;
+  */
+
+  return <></>;
+};
