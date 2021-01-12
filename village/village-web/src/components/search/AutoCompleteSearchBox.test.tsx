b85516f41520f4b440d9cdb6c7eb5f34981de176
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-01-12 13:20:39 -0800

    WIP for unit tests

--- village/village-web/src/components/search/AutoCompleteSearchBox.test.tsx
+++ village/village-web/src/components/search/AutoCompleteSearchBox.test.tsx
@@ -1,7 +1,25 @@
-import { render } from "@testing-library/react";
-import React from "react";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React, { forwardRef, useImperativeHandle } from "react";
+import {
+  SearchSuggestionFetchFn,
+  SearchSuggestion,
+  matchesToSearchSuggestions,
+} from "../../services/search/Suggestions";
+import { UserPost } from "../../services/store/Posts";
 import { UserRecord } from "../../services/store/Users";
-import { AutocompleteSearchBox } from "./AutocompleteSearchBox";
+import {
+  AutocompleteSearchBox,
+  AUTOCOMPLETE_SEARCH_BOX_PROMPT,
+  useAutocompleteSearchBoxDialog,
+} from "./AutocompleteSearchBox";
+import { getCreateNewPostPrompt } from "./CreateNewPostSearchResult";
 
 const user0: UserRecord = {
   uid: "123",
@@ -19,4 +37,60 @@ test("renders AutocompleteSearchBox", () => {
       createPost={jest.fn()}
     />
   );
+});
+
+test("useAutocompleteSearchBoxDialog set up components correctly", async () => {
+  const getSuggestions = jest.fn(async (value: string) => {
+    console.log(value);
+    const matches: UserPost[] = [];
+    const suggestions = matchesToSearchSuggestions(matches, value);
+    console.log(suggestions);
+    return suggestions;
+  });
+
+  const createPost = jest.fn();
+
+  const TestComponent = forwardRef((props, ref) => {
+    const {
+      dialogOpen,
+      setDialogOpen,
+      dialog,
+    } = useAutocompleteSearchBoxDialog(user0, createPost, getSuggestions);
+
+    useImperativeHandle(ref, () => ({
+      setDialogOpen: (open: boolean) => setDialogOpen(open),
+    }));
+
+    return dialog;
+  });
+
+  const ref = {
+    current: { setDialogOpen: jest.fn() },
+  };
+
+  const { container } = render(<TestComponent ref={ref} />);
+
+  // Dialog not yet rendered.
+  expect(container).toBeEmptyDOMElement();
+
+  // Dialog should be rendered.
+  act(() => ref.current.setDialogOpen(true));
+  const inputEl = screen.getByPlaceholderText(AUTOCOMPLETE_SEARCH_BOX_PROMPT);
+  expect(inputEl).toBeInTheDocument();
+
+  // getsuggestions should be called
+  const mockSearchBoxInput = "baz";
+  // await act(() => userEvent.type(inputEl, mockSearchBoxInput));
+  userEvent.type(inputEl, mockSearchBoxInput);
+  waitFor(() => expect(inputEl).toHaveTextContent(mockSearchBoxInput));
+  expect(getSuggestions).toHaveBeenCalled();
+
+  // the option to create post should be present
+  const createNewPostPromptText = getCreateNewPostPrompt(mockSearchBoxInput);
+  const createNewPostSearchResultEl = screen.getByText(createNewPostPromptText);
+  expect(createNewPostSearchResultEl).toBeInTheDocument();
+
+  // Progress indicator should be rendered
+
+  // Dialog should be rendered again
 });
--- village/village-web/src/components/search/AutocompleteSearchBox.tsx
+++ village/village-web/src/components/search/AutocompleteSearchBox.tsx
@@ -29,6 +29,8 @@ const AUTOCOMPLETE_SEARCH_BOX_KEYMODIFIER = "command";
 export const AUTOCOMPLETE_SEARCH_BOX_HOTKEY = `${AUTOCOMPLETE_SEARCH_BOX_KEYMODIFIER}+${AUTOCOMPLETE_SEARCH_BOX_KEY}`;
 export const AUTOCOMPLETE_SEARCH_BOX_ESCKEY = "escape";
 
+export const AUTOCOMPLETE_SEARCH_BOX_PROMPT = "Type to search and create";
+
 const isAutocompleteSearchBoxHotkey = (event: React.KeyboardEvent) => {
   if (AUTOCOMPLETE_SEARCH_BOX_KEYMODIFIER !== "command") {
     // needs to match with key-matching logic in this function. This is simpler
@@ -100,6 +102,7 @@ export const AutocompleteSearchBox = (props: AutocompleteSearchBoxProps) => {
 
   const getSuggestionValue = (suggestion: SearchSuggestion) => suggestion.title;
   const renderSuggestion = (suggestion: SearchSuggestion) => {
+    console.log("WTF");
     switch (suggestion.action) {
       case "createNewPost":
         return <CreateNewPostSearchResult title={suggestion.title} />;
@@ -167,7 +170,7 @@ export const AutocompleteSearchBox = (props: AutocompleteSearchBoxProps) => {
         props.onClose();
       }
     },
-    placeholder: "Type to search and create",
+    placeholder: AUTOCOMPLETE_SEARCH_BOX_PROMPT,
     value: value,
     onChange: onChange,
     ref: inputRef,
