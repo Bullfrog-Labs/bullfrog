e4b0ac3dabc10b8b4b5aabfb72b58666e4c4ce0d
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-01-14 12:25:17 -0800

    ProfileView was erroring because it was not wrapped in a router

--- village/village-web/src/stories/ProfileView.stories.tsx
+++ village/village-web/src/stories/ProfileView.stories.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import { ProfileView, ProfileViewProps } from "../components/ProfileView";
 import { u0, posts0 } from "../testing/Fixtures";
+import { MemoryRouter } from "react-router-dom";
 
 export default {
   title: "Village/ProfileView",
   component: ProfileView,
 } as Meta;
 
-const Template: Story<ProfileViewProps> = (args) => <ProfileView {...args} />;
+const Template: Story<ProfileViewProps> = (args) => (
+  <MemoryRouter>
+    <ProfileView {...args} />
+  </MemoryRouter>
+);
 
 export const Default = Template.bind({});
 Default.args = {
