f0bca4ab704b26e93e7c116f140bfcf272cdb066
Leigh Stewart <leigh@macbook-pro.lan>
2021-01-21 10:08:03 -0800

    add story back

---
+++ village/village-web/src/components/identity/PostAuthorLink.stories.tsx
@@ -0,0 +1,47 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { MemoryRouter } from "react-router-dom";
+import { PostAuthorLink, PostAuthorLinkProps } from "./PostAuthorLink";
+
+export default {
+  title: "Identity/PostAuthorLink",
+  component: PostAuthorLink,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+} as Meta;
+
+const Template: Story<PostAuthorLinkProps> = (args) => (
+  <PostAuthorLink {...args} />
+);
+
+export const UserIsAuthor = Template.bind({});
+UserIsAuthor.args = {
+  viewer: {
+    uid: "123",
+    displayName: "foo",
+    username: "foo",
+  },
+  author: {
+    uid: "123",
+    displayName: "foo",
+    username: "foo",
+  },
+};
+
+export const UserIsNotAuthor = Template.bind({});
+UserIsNotAuthor.args = {
+  viewer: {
+    uid: "123",
+    displayName: "foo",
+    username: "foo",
+  },
+  author: {
+    uid: "456",
+    displayName: "bar",
+    username: "bar",
+  },
+};
