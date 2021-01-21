288164d77bbbcdb5fcfe00aba1211c5d44eb38da
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-01-20 21:57:38 -0800

    useMentions test WIP

--- village/village-web/src/hooks/useMentions.test.tsx
+++ village/village-web/src/hooks/useMentions.test.tsx
@@ -2,15 +2,14 @@ import * as log from "loglevel";
 import { useMentions } from "./useMentions";
 import { act, renderHook } from "@testing-library/react-hooks";
 import { Logging } from "kmgmt-common";
-import { CreatePostResult, UserPost } from "../services/store/Posts";
-import { Body } from "../components/richtext/RichTextEditor";
+import { CreatePostFn, UserPost } from "../services/store/Posts";
 import { userPosts0, userPosts1, u0 } from "../testing/Fixtures";
 
 Logging.configure(log);
 
 test("fetches empty mentions", async () => {
   const { result } = renderHook(() =>
-    useMentions(getGlobalMentions0, createPost0, authorId, authorUsername)
+    useMentions(getGlobalMentions0, createPost0, authorUsername)
   );
 
   var [mentionables] = result.current;
@@ -20,13 +19,13 @@ test("fetches empty mentions", async () => {
 
 test("creates suggested mention when no own user match exists", async () => {
   const { waitForNextUpdate, result } = renderHook(() =>
-    useMentions(getGlobalMentions0, createPost0, authorId, authorUsername)
+    useMentions(getGlobalMentions0, createPost0, authorUsername)
   );
 
   var [mentionables, onMentionSearchChanged] = result.current;
 
   await act(async () => {
-    onMentionSearchChanged("wabisabi");
+    onMentionSearchChanged(authorId)("wabisabi");
     await waitForNextUpdate();
   });
 
@@ -38,7 +37,7 @@ test("creates suggested mention when no own user match exists", async () => {
 
 test("creates suggested mention when only other user match exists", async () => {
   const { waitForNextUpdate, result } = renderHook(() =>
-    useMentions(getGlobalMentions1, createPost0, u0.uid, u0.username)
+    useMentions(getGlobalMentions1, createPost0, u0.username)
   );
 
   var [mentionables, onMentionSearchChanged] = result.current;
@@ -56,7 +55,7 @@ test("creates suggested mention when only other user match exists", async () => 
 
 test("fetches non empty mentions", async () => {
   const { waitForNextUpdate, result } = renderHook(() =>
-    useMentions(getGlobalMentions0, createPost0, authorId, authorUsername)
+    useMentions(getGlobalMentions0, createPost0, authorUsername)
   );
 
   var [mentionables, onMentionSearchChanged] = result.current;
@@ -77,18 +76,13 @@ test("fetches non empty mentions", async () => {
   });
 });
 
-const getGlobalMentions0 = async (titlePrefix: string): Promise<UserPost[]> => {
+const getGlobalMentions0 = async (): Promise<UserPost[]> => {
   return userPosts0;
 };
-const getGlobalMentions1 = async (titlePrefix: string): Promise<UserPost[]> => {
+const getGlobalMentions1 = async (): Promise<UserPost[]> => {
   return userPosts1;
 };
-const createPost0 = async (
-  title: string,
-  body: Body,
-  postId?: string
-): Promise<CreatePostResult> => {
+const createPost0: CreatePostFn = async () => {
   return { state: "success", postId: "hjkhj", postUrl: "" };
 };
-const authorId = "79832475341985234";
 const authorUsername = "donkeyKong";
