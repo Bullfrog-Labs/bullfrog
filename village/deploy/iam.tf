e90d2b343a8389e4d91854d2a992593cc4806b1b
Leigh Stewart <leigh@macbook-pro.lan>
2021-01-26 10:27:42 -0800

    add files

---
+++ village/deploy/iam.tf
@@ -0,0 +1,17 @@
+locals {
+  cloud_build_member = "serviceAccount:${google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "firebase_admin" {
+  role   = "roles/firebase.admin"
+  member = local.cloud_build_member
+
+  depends_on = [google_project_service.cloud_build]
+}
+
+resource "google_project_iam_member" "api_keys_admin" {
+  role   = "roles/serviceusage.apiKeysViewer"
+  member = local.cloud_build_member
+
+  depends_on = [google_project_service.cloud_build]
+}
---
+++ village/deploy/vars.tf
@@ -0,0 +1,8 @@
+variable "project" {}
+variable "user" {}
+variable "region" {}
+variable "firebase_location" {}
+
+variable "billing_account" {
+  description = "Bullfrog Labs"
+}
