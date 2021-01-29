44ab3d306039c8607bf5f40a0d2409b779468411
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-01-29 14:26:39 -0800

    Twitter auth cleanup (#103)
    - Removing the Twitter cred merging code.
    - Disabling Google auth.

--- village/village-web/src/components/auth/FirebaseAuthComponent.tsx
+++ village/village-web/src/components/auth/FirebaseAuthComponent.tsx
@@ -49,10 +49,7 @@ export const FirebaseAuthComponent: FunctionComponent<FirebaseAuthComponentProps
     return <Redirect to={postLoginRedirect.from.pathname} />;
   } else {
     const config: firebaseui.auth.Config = {
-      signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      ],
+      signInOptions: [firebase.auth.TwitterAuthProvider.PROVIDER_ID],
       signInFlow: "popup",
       callbacks: {
         signInSuccessWithAuthResult: (
