40346ed32ac4ef143f5165eb0d26a84376637120
Leigh Stewart <leigh@macbook-pro.lan>
2021-01-29 10:55:38 -0800

    fix imports

--- village/village-web/src/components/StackView.tsx
+++ village/village-web/src/components/StackView.tsx
@@ -2,7 +2,7 @@ import * as log from "loglevel";
 import React, { useState, useEffect } from "react";
 import { Container, Typography } from "@material-ui/core";
 import Divider from "@material-ui/core/Divider";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import { useParams } from "react-router-dom";
