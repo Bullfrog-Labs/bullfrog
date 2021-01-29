6d33f67f019259cbc916230784d4d637708bb690
Leigh Stewart <leighst@users.noreply.github.com>
2021-01-29 11:12:49 -0800

    fix prod style issue (#102)

--- village/village-web/src/components/AppContainer.tsx
+++ village/village-web/src/components/AppContainer.tsx
@@ -5,7 +5,7 @@ import {
   Drawer,
   MuiThemeProvider,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core";
 import React from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 import {
