0d6e8fa687d6611d810ddb2e28beda63272b5ccb
Leigh Stewart <leighst@users.noreply.github.com>
2021-03-04 08:19:40 -0800

    public: remove some unneeded login code (#120)

--- village/village-web/src/components/auth/FirebaseAuthComponent.tsx
+++ village/village-web/src/components/auth/FirebaseAuthComponent.tsx
@@ -1,14 +1,9 @@
 import firebase from "firebase/app";
 import firebaseui from "firebaseui";
 import * as log from "loglevel";
-import React, { FunctionComponent, useContext } from "react";
+import React, { FunctionComponent } from "react";
 import { StyledFirebaseAuth } from "react-firebaseui";
-import { Redirect, useHistory, useLocation } from "react-router-dom";
-import {
-  AppAuthContext,
-  useAppAuthStatusFromAppAuthContext,
-  useIsLoggedIn,
-} from "../../services/auth/AppAuth";
+import { useLocation } from "react-router-dom";
 import FirebaseAuthProvider from "../../services/auth/FirebaseAuthProvider";
 
 interface LocationState {
@@ -41,61 +36,35 @@ export const FirebaseAuthComponent: FunctionComponent<FirebaseAuthComponentProps
     },
   };
 
-  const appAuthState = useAppAuthStatusFromAppAuthContext();
-  const redirectIsPrivate = postLoginRedirect.isPrivate;
-
-  const userIsLoggedIn = useIsLoggedIn();
-  const userCanAccessRedirect = redirectIsPrivate === userIsLoggedIn;
-
-  const shouldRedirectToSignup =
-    appAuthState.state === "not-whitelisted" && redirectIsPrivate;
-
-  const authCompleted = useContext(AppAuthContext).authCompleted;
-
-  if (
-    authCompleted &&
-    userCanAccessRedirect &&
-    // If the user is trying to login for the first time, we should not redirect.
-    !(appAuthState.state === "not-logged-in")
-  ) {
-    // short-circuit because authState has been populated and there is no
-    // need to get the user to authenticate manually via the auth component.
-    logger.debug(`redirecting to ${postLoginRedirect.from.pathname}`);
-    return <Redirect to={postLoginRedirect.from.pathname} />;
-  } else if (authCompleted && shouldRedirectToSignup) {
-    logger.debug(`redirecting to /signup because user is not whitelisted`);
-    return <Redirect to={"/signup"} />;
-  } else {
-    const signInOptions = [firebase.auth.TwitterAuthProvider.PROVIDER_ID];
-    if (googleAuthEnabled) {
-      signInOptions.push(firebase.auth.GoogleAuthProvider.PROVIDER_ID);
-    }
-    const config: firebaseui.auth.Config = {
-      signInOptions: signInOptions,
-      signInFlow: "popup",
-      callbacks: {
-        signInSuccessWithAuthResult: (
-          authResult: firebase.auth.UserCredential,
-          redirectUrl
-        ) => {
-          if (!authResult.user) {
-            throw new Error(
-              "Authed user should not be null after successful signin"
-            );
-          }
-
-          logger.debug(
-            `successful signin, got user ${authResult.user.uid} : ${authResult.user.displayName}`
-          );
-
-          return false;
-        },
-      },
-      signInSuccessUrl: postLoginRedirect.from.pathname,
-    };
-
-    return (
-      <StyledFirebaseAuth uiConfig={config} firebaseAuth={authProvider.auth} />
-    );
+  const signInOptions = [firebase.auth.TwitterAuthProvider.PROVIDER_ID];
+  if (googleAuthEnabled) {
+    signInOptions.push(firebase.auth.GoogleAuthProvider.PROVIDER_ID);
   }
+  const config: firebaseui.auth.Config = {
+    signInOptions: signInOptions,
+    signInFlow: "popup",
+    callbacks: {
+      signInSuccessWithAuthResult: (
+        authResult: firebase.auth.UserCredential,
+        redirectUrl
+      ) => {
+        if (!authResult.user) {
+          throw new Error(
+            "Authed user should not be null after successful signin"
+          );
+        }
+
+        logger.debug(
+          `successful signin, got user ${authResult.user.uid} : ${authResult.user.displayName}`
+        );
+
+        return false;
+      },
+    },
+    signInSuccessUrl: postLoginRedirect.from.pathname,
+  };
+
+  return (
+    <StyledFirebaseAuth uiConfig={config} firebaseAuth={authProvider.auth} />
+  );
 };
