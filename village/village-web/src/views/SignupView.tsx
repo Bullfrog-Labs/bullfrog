b64ec1fa2c826b4e214d52a89f62b976407f51b3
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-03-04 15:57:52 -0800

    Changing SignupView to work with subdomain-based hosting of Leadpages (#126)

--- village/village-web/src/views/SignupView.tsx
+++ village/village-web/src/views/SignupView.tsx
@@ -1,25 +1,10 @@
 import React from "react";
-import { Helmet } from "react-helmet";
 
 interface SignupViewProps {}
 
 export const SignupView: React.FunctionComponent<SignupViewProps> = (
   props = {}
 ) => {
-  return (
-    <>
-      <Helmet>
-        <title>Sign up for Village</title>
-        <meta
-          property="og:url"
-          content="https://villageink.lpages.co/village-signup/"
-        />
-        <link
-          rel="opengraph"
-          href="https://villageink.lpages.co/village-signup/"
-        />
-        <script src="//villageink.lpages.co/_/js/village-signup/"></script>
-      </Helmet>
-    </>
-  );
+  window.location.href = "https://signup.village.ink";
+  return <></>;
 };
