18a7189c8358bdff44fabb798f466ebc82e3c2a4
Leigh Stewart <leigh@macbook-pro.lan>
2021-03-05 10:32:12 -0800

    remove caching for now

--- village/functions/src/index.ts
+++ village/functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 import { lookupTwitterUserById } from "./Twitter";
-import fs from "fs";
 import express from "express";
+import axios from "axios";
 
 const prerender = require("prerender-node");
 
@@ -22,19 +22,48 @@ export const lookupTwitterUser = functions.https.onCall(
   }
 );
 
+const fetchIndexHtml = async () => {
+  let indexHtml;
+
+  const startTimeMs = Date.now();
+  try {
+    const rep = await axios.get(`http://village.ink/iandex.html`);
+    if (rep.status !== 200) {
+      console.log(`Error fetching index.html; status=${rep.status}`);
+      indexHtml = undefined;
+    } else {
+      indexHtml = rep.data;
+    }
+  } catch (e) {
+    console.log(`Error fetching index.html; e=${e}`);
+    indexHtml = undefined;
+  }
+  console.log(`Fetch complete; timeMs=${Date.now() - startTimeMs}`);
+
+  return indexHtml;
+};
+
 prerender.set("prerenderToken", "Jn7KZQLHcmqfEkDMvY0l");
 const app = express();
 app.use((req: any, res: any, next: any) => {
-  console.log(`before`);
+  console.log(`Before prerender`);
   next();
 });
 app.use(prerender);
 app.use((req: any, res: any, next: any) => {
-  console.log(`after`);
+  console.log(`After prerender`);
   next();
 });
-app.get("*", (req, res) => {
-  res.status(200).send(fs.readFileSync("./www/index.html").toString());
+app.get("*", async (req, res) => {
+  const indexHtml = await fetchIndexHtml();
+  if (indexHtml) {
+    res
+      .set("Cache-Control", "public, max-age=600")
+      .status(200)
+      .send(indexHtml.toString());
+  } else {
+    res.status(500).send();
+  }
 });
 
 export const proxy = functions.https.onRequest(app);
