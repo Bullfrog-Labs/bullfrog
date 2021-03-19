0a26aef32bbb273452d6120caa27f58441ac4f84
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-03-18 18:31:26 -0700

    Size issue is sorted

--- village/village-web/src/components/notifications/NotificationsList.tsx
+++ village/village-web/src/components/notifications/NotificationsList.tsx
@@ -1,39 +1,38 @@
+import { makeStyles } from "@material-ui/core";
 import React, { CSSProperties } from "react";
-import { FixedSizeList } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
+import { FixedSizeList } from "react-window";
 
 const Row = (args: { index: number; style: CSSProperties }) => {
   return <div style={args.style}>Row {args.index}</div>;
 };
 
-const NOTIFICATION_LIST_DEFAULT_HEIGHT = 1000;
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: "100vh",
+  },
+}));
 
 export type NotificationsListProps = {};
 
 export const NotificationsList = (props: NotificationsListProps) => {
+  const classes = useStyles();
   return (
-    <div style={{ height: "100%", overflowX: "hidden" }}>
-      <div style={{ display: "flex", height: "100%" }}>
-        <div style={{ flex: "1 1 auto", height: "100%" }}>
-          <AutoSizer disableHeight>
-            {({ height, width }) => {
-              console.log(height);
-              console.log(window.innerHeight);
-              const actualHeight = height ?? NOTIFICATION_LIST_DEFAULT_HEIGHT;
-              return (
-                <FixedSizeList
-                  itemSize={60}
-                  itemCount={1000}
-                  height={actualHeight}
-                  width={width}
-                >
-                  {Row}
-                </FixedSizeList>
-              );
-            }}
-          </AutoSizer>
-        </div>
-      </div>
+    <div className={classes.container}>
+      <AutoSizer>
+        {({ height, width }) => {
+          return (
+            <FixedSizeList
+              itemSize={60}
+              itemCount={1000}
+              height={height}
+              width={width}
+            >
+              {Row}
+            </FixedSizeList>
+          );
+        }}
+      </AutoSizer>
     </div>
   );
 };
