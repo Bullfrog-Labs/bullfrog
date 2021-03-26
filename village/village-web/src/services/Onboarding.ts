f3b268ab612136b5c1c38440b1c52354c9f94c4e
Leigh Stewart <leigh@macbook-pro.lan>
2021-03-26 16:05:14 -0700

    fix onboarding doc

--- village/village-web/src/services/Onboarding.ts
+++ village/village-web/src/services/Onboarding.ts
@@ -1,5 +1,52 @@
 import { RichText } from "../components/richtext/Types";
 
+/**
+ * This json doc is what is saved as the new user onboarding welcome post.
+ * The best way to modify this is to create a post with the content you
+ * want, print the children of the slate doc in the console, and copy it
+ * out.
+ *
+ * Keep this doc simple - old versions of it wont be updated when it changes.
+ */
 export const ONBOARDING_POST_BODY: RichText = [
-  { children: [{ children: [{ text: "" }], type: "p" }] },
+  {
+    children: [
+      {
+        children: [{ text: "Hello and welcome to Village 🎉🎉🎉!" }],
+        type: "p",
+      },
+      {
+        type: "p",
+        children: [
+          { text: "For information on how to get started, check out " },
+          {
+            type: "mention",
+            children: [{ text: "" }],
+            value: "Getting Started in Village",
+            postId: "iUJS6XHlr57lkYsfTpxn",
+            authorId: "XIyXiG6v38Ru7sQarPqYtlIGolC3",
+            authorUsername: "getvillageink",
+            exists: true,
+          },
+          { text: "." },
+        ],
+      },
+      {
+        type: "p",
+        children: [
+          { text: "For other help & support, have a look at " },
+          {
+            type: "mention",
+            children: [{ text: "" }],
+            value: "Help & Support",
+            postId: "L0SdDtT3OwdXMOGymI4f",
+            authorId: "XIyXiG6v38Ru7sQarPqYtlIGolC3",
+            authorUsername: "getvillageink",
+            exists: true,
+          },
+          { text: "." },
+        ],
+      },
+    ],
+  },
 ];
