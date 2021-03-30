0502f75158da23a452f463867aa36e9cc972c664
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-03-30 16:35:23 -0700

    Moved the richTextToMarkdown function to the right palce

--- village/village-web/src/components/richtext/Utils.ts
+++ village/village-web/src/components/richtext/Utils.ts
@@ -1,7 +1,10 @@
 import { ELEMENT_MENTION } from "@blfrg.xyz/slate-plugins";
-import * as _ from "lodash";
+import ld, * as _ from "lodash";
 import * as log from "loglevel";
-import { createEditor, Editor, Node, Path } from "slate";
+import { serialize } from "remark-slate";
+import { NodeTypes } from "remark-slate/dist/deserialize";
+import { BlockType, LeafType } from "remark-slate/dist/serialize";
+import { createEditor, Editor, Element, Node, Path, Text } from "slate";
 import { PostId, UserPost } from "../../services/store/Posts";
 import { RichText, RichTextNode } from "./Types";
 
@@ -219,4 +222,102 @@ export const findMentionsInPosts = (
 // This is not totally correct. It wouldn't "see" mentions etc.
 export const isEmptyDoc = (doc: RichText): boolean => {
   return _.isEqual(doc, EMPTY_RICH_TEXT);
+};
+
+const NODE_TYPES: NodeTypes = {
+  paragraph: "p",
+  block_quote: "blockquote",
+  code_block: "code_block",
+  link: "a",
+  ul_list: "ul",
+  ol_list: "ol",
+  listItem: "li",
+  heading: {
+    1: "h1",
+    2: "h2",
+    3: "h3",
+    4: "h4",
+    5: "h5",
+    6: "h6",
+  },
+  emphasis_mark: "italic",
+  strong_mark: "bold",
+  delete_mark: "strikethrough",
+  thematic_break: "thematic_break",
+};
+
+const TO_MARKDOWN_OPTIONS = { nodeTypes: NODE_TYPES };
+
+const mentionsToBracketedRefs = (richText: RichText) => {
+  type QueueElement = [Element | undefined, number, RichTextNode];
+  type Queue = QueueElement[];
+
+  const pushOntoQueue = (
+    queue: Queue,
+    parent: Element | undefined,
+    children: RichTextNode[]
+  ) => {
+    let idx = 0;
+    children.forEach((child) => {
+      queue.push([parent, idx, child]);
+      idx++;
+    });
+  };
+
+  const findAllMentions = () => {
+    const allMentions: Queue = [];
+    const traversalQueue: Queue = [];
+    pushOntoQueue(traversalQueue, undefined, richText);
+
+    while (true) {
+      const next = traversalQueue.pop();
+      if (!next) {
+        break;
+      }
+
+      const [parent, idx, element] = next;
+
+      if (!!element.type && element.type === "mention") {
+        allMentions.push(next);
+      }
+
+      if (!element.children) {
+        continue;
+      }
+
+      const nonLeaf = element as Element;
+      pushOntoQueue(traversalQueue, nonLeaf, nonLeaf.children);
+    }
+
+    return allMentions;
+  };
+
+  const allMentions = findAllMentions();
+
+  allMentions.forEach((element: QueueElement) => {
+    const [parent, idx, mention] = element;
+    // not handled as link because the existing links would be invalid
+    // off-platform.
+    parent!.children[idx] = { text: `[[${mention.value}]]` } as Text;
+    // remove the "" added for cursor on either side of mention
+    parent!.children.splice(idx - 1, 1);
+    parent!.children.splice(idx, 1);
+  });
+};
+
+export const richTextToMarkdown = (richText: RichText) => {
+  // Copy input since this function destructively modifies it.
+  const rtClone = ld.cloneDeep(richText);
+
+  // Modify mentions here so that they are handled properly by remark-slate
+  // serialize.
+  mentionsToBracketedRefs(rtClone);
+
+  const result = rtClone
+    .map((chunk) =>
+      serialize(chunk as BlockType | LeafType, TO_MARKDOWN_OPTIONS)
+    )
+    .join("");
+
+  return result;
 };
